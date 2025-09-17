7cbb9f96f0ec5a128372fbf2e2eb37de68d15df2
Philipp Memmel <philipp.memmel@gmail.com>
2025-09-17 06:20:27 +0200

    Add GHA release workflow, bump version to v2.1

--- version.php
+++ version.php
@@ -27,7 +27,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'tiny_ai';
 $plugin->release = 'v2.1';
-$plugin->version = 2025073100;
+$plugin->version = 2025091700;
 $plugin->requires = 2025041400;
 $plugin->maturity = MATURITY_BETA;
 $plugin->dependencies = [
